4e1b2ae917b93a6d14bba921b57036cf3de78d74
Alec Cooper <AlecCooper@users.noreply.github.com>
2021-01-23 20:30:16 -0800

    string list

---
+++ string_list.py
@@ -0,0 +1,19 @@
+
+import operator
+
+
+def merge(original, add, remove):
+
+    results = (set(original) | set(add)) - set(remove)
+
+    results = list(results)
+
+    results.sort(key = lambda x: (len(x), x), reverse=True)
+
+    return results
+
+
+print(merge(
+    ["one", "two", "three"], 
+    ["one", "two", "five", "six"],
+    ["two", "five"]))
